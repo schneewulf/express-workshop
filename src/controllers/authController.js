9604b3acf542ebd2897bc9cca7000d51877f541d
broerjuang <juangwiantoro@gmail.com>
2018-02-23 14:46:59 +0700

    v4

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,5 +1,50 @@
 // @flow
+import type {$Request as Req, $Response as Res} from 'express';
+import bcrypt from 'bcrypt';
 
-function singUpController() {}
+type ReqBody = {
+  name: string,
+  email: string,
+  password: string,
+  confirmationPassword: string,
+};
+
+type ExtReq = {body: ReqBody} & Req;
+
+async function signupController(req: ExtReq, res: Res) {
+  let {name, email, password, confirmationPassword} = req.body;
+  if (
+    name === undefined ||
+    email === undefined ||
+    password === undefined ||
+    confirmationPassword === undefined
+  ) {
+    res.status(400).json({
+      status: 'ERROR',
+      message:
+        'Either email or password or name or confirmation password is not sent to the server',
+    });
+  } else if (password !== confirmationPassword) {
+    res.status(400).json({
+      status: 'ERROR',
+      message: 'password and confirmation password do not matched',
+    });
+  } else {
+    try {
+      let salt = bcrypt.genSaltSync(10);
+      let hashedPassword = bcrypt.hashSync(password, salt);
+      res.status(200).json({
+        status: 'OK',
+        message: req.body,
+        password: hashedPassword,
+      });
+    } catch (err) {
+      res.status(500).json({
+        status: 'ERROR',
+        message: 'NaNNaNNaNNaN Batman!',
+      });
+    }
+  }
+}
 
 export {signupController};
--- src/main.js
+++ src/main.js
@@ -4,7 +4,15 @@ import express from 'express';
 import mainRoute from './routes/mainRoute';
 import {PORT} from './globals/config';
 
+import bodyParser from 'body-parser';
+
 let app = express();
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: false}));
+
+// parse application/json
+app.use(bodyParser.json());
 
 app.use('/api', mainRoute);
 
